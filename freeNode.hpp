0f6d5291dd30d6bfc8b43bcb51c96c941819401e
Abhishek S YELLANKI <abhishekys1993@gmail.com>
2017-09-17 20:33:21 -0400

    Definition of free inode, not used by any process

---
+++ freeNode.hpp
@@ -0,0 +1,16 @@
+#ifndef _FREENODE_H_
+#define _FREENODE_H_
+
+#include <list>
+#include <sys/types.h>
+
+class FreeNode{
+    public:
+        uint num_blocks;
+        uint pos;
+        FreeNode(uint num_blocks, uint pos) 
+            : num_blocks(num_blocks), 
+            pos(pos) {}
+};
+
+#endif
