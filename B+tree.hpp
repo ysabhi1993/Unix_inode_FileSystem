170b5608a464de7770d28d10e1423f7632fd3164
Abhishek S YELLANKI <abhishekys1993@gmail.com>
2017-10-11 16:44:47 -0400

    searching, indexing files using B+ trees

--- B+tree.hpp
+++ B+tree.hpp
@@ -1,11 +1,16 @@
 #ifndef _BPLUSTREENODE_H_
 #define _BPLUSTREENODE_H_
 
+#define TREE_PREFIX "leaves/leaf_"
+
+#include "FileObject.hpp"
+
 #include <iostream>
 #include <sys/types.h>
 #include <algorithm>
 #include <climits>
 #include <cmath> 
+#include <fstream>
 #include <string>
 #include <iostream>
 #include <memory>
@@ -38,18 +43,18 @@ class TreeNode{
     public:
         TreeNode();
         TreeNode(long _fileIndex);              //Given a fileIndex, read it
-        bool isLeaf();          //Check if leaf
-        string getFileName(); //Get the file name
-        long getFileIndex(); //Get the fileIndex
-        void setToInternalNode();  //set to internalNode
-        long size(); //Return the size of keys
+        bool isLeaf() { return leaf; }          //Check if leaf
+        string getFileName() { return TREE_PREFIX + to_string(fileIndex); } //Get the file name
+        long getFileIndex() { return fileIndex; } //Get the fileIndex
+        void setToInternalNode() { leaf = false; }  //set to internalNode
+        long size() { return keys.size(); } //Return the size of keys
         static void initialize(); //Initialize the for the tree
         long getKeyPosition(double key); //Return the position of a key in keys
         void commitToDisk(); //Commit node to disk
         void readFromDisk(); //Read from the disk into memory
         void printNode(); //Print node information
         void serialize(); //Serialize the subtree
-        //void insertObject(DBObject object); //Insert object into disk
+        void insertObject(FileObject object); //Insert object into disk
         void insertNode(double key, 
                         long leftChildIndex, 
                         long rightChildIndex); //Insert an internal node into the tree
